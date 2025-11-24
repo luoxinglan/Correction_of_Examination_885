9ca55b83f4ff064bdcfe499ac5d4ef4137d76020
heihuhu_tianxuan <liugangfuha@163.com>
2025-11-24 09:43:42 +0800

    初始化11.24。

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * 当你想分享你的项目时，必须将其推送到上游。
+ * 这个命令很简单：git push <remote> <branch>。
+ * 当你想要将 master 分支推送到 origin 服务器时（再次说明，克隆时通常会自动帮你设置好那两个名字），
+ * 那么运行这个命令就可以将你所做的备份到服务器：
+ * $ git push origin master
+ */
 
 int main(void) {
     printf("Hello, World!\n");
-    //
     return 0;
 }
